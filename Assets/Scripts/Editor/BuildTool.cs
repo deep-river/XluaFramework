24eae0ad5941f42ff7e51a7ce21f09ad4a408c80
Bangyu Li <by.brad.li@gmail.com>
2023-05-25 17:21:29 +0800

    Init repository

---
+++ Assets/Scripts/Editor/BuildTool.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildTool : Editor
+{
+    [MenuItem("Tools/Build Windows Bundle")]
+    static void BundleWindowsBuild()
+    {
+        Build(BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("Tools/Build Android Bundle")]
+    static void BundleAndroidBuild()
+    {
+        Build(BuildTarget.Android);
+    }
+
+    [MenuItem("Tools/Build IOS Bundle")]
+    static void BundleIOSBuild()
+    {
+        Build(BuildTarget.iOS);
+    }
+
+    static void Build(BuildTarget targetPlatform)
+    {
+        List<AssetBundleBuild> assetBundleBuilds = new List<AssetBundleBuild>();
+        string[] files = Directory.GetFiles(PathUtil.BuildResourcesPath, "*", SearchOption.AllDirectories);
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            if (files[i].EndsWith(".meta"))
+                continue;
+            AssetBundleBuild assetBundle = new AssetBundleBuild();
+
+            string fileName = PathUtil.GetStandardPath(files[i]);
+            Debug.Log("file:" + fileName);
+
+            string assetName = PathUtil.GetUnityPath(fileName);
+            assetBundle.assetNames = new string[] { assetName };
+            string bundleName = fileName.Replace(PathUtil.BuildResourcesPath, "").ToLower();
+            assetBundle.assetBundleName = bundleName + ".ab";
+            assetBundleBuilds.Add(assetBundle);
+        }
+
+        if (Directory.Exists(PathUtil.BundleOutPath))
+            Directory.Delete(PathUtil.BundleOutPath, true);
+        Directory.CreateDirectory(PathUtil.BundleOutPath);
+
+        BuildPipeline.BuildAssetBundles(PathUtil.BundleOutPath, assetBundleBuilds.ToArray(), BuildAssetBundleOptions.None, targetPlatform);
+    }
+}
